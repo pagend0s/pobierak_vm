0e24b85bdf0a016d2f5dfde879560915b0283c5c
pagend0s <65870887+pagend0s@users.noreply.github.com>
2022-09-05 21:03:09 +0200

    Update python_install.sh

--- python_install.sh
+++ python_install.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt-get install wget build-essential checkinstall
+sudo apt-get install wget build-essential checkinstall -y
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
 
 
