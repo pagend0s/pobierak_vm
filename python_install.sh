9065a21a099b3bae9c97a7681411967e8015e9b9
pagend0s <65870887+pagend0s@users.noreply.github.com>
2022-09-05 12:24:01 +0200

    Script that automates installation of Python 3.9.9
    The latest version of YOUTUBE-DLP requires Python 3.7 or higher.

---
+++ python_install.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install wget build-essential checkinstall
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
+
+
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz
+
+sudo tar xzf Python-3.9.9.tgz
+
+cd Python-3.9.9
+sudo ./configure --enable-optimizations
+sudo make altinstall
